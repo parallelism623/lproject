bddb06787c6d1167c741bfddacbfd629e140e1ce
Hieu Truong Anh <hieu.truonganh@nashtechglobal.com>
2025-11-05 09:58:17 +0700

    add health check

---
+++ lproject.Lib/HealthCheckPublishers/DefaultHealthCheckPublisher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace lproject.Lib.HealthCheckPublishers;
+public class DefaultHealthCheckPublisher : IHealthCheckPublisher
+{
+    public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Test");
+        return Task.CompletedTask;
+    }
+}
