550b23f2c1c0d3f4b29b1fb50c20af10810228c7
Hieu Truong Anh <hieu.truonganh@nashtechglobal.com>
2025-11-05 09:58:17 +0700

    initial dev branch

---
+++ lproject.Lib/HealthChecks/AppHealthCheckPublisher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace lproject.Lib.HealthChecks;
+public class AppHealthCheckPublisher : IHealthCheckPublisher
+{
+    public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("App");
+        return Task.CompletedTask;
+    }
+}
