fee2b11b538f7415f01b9dc9161aa622a66014d6
Hieu Truong Anh <hieu.truonganh@nashtechglobal.com>
2025-11-06 12:41:39 +0700

    add ui

--- lproject.API/Program.cs
+++ lproject.API/Program.cs
@@ -1,21 +1,22 @@
-using lproject.Lib.HealthCheckPublishers;
+﻿using HealthChecks.UI.Client;
 using lproject.Lib.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHealthChecks()
-    .AddCheck<DefaultHealthChecks>("Default", tags: ["sample"]);
-builder.Services.AddControllers();
+    .AddCheck<DefaultHealthChecks>("Default", tags: ["sample"])
+    .AddRedis("localhost:6379", name: "redis");
+builder.Services.AddHealthChecksUI()
+    .AddInMemoryStorage();
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Services.Configure<HealthCheckPublisherOptions>(options =>
 {
     options.Delay = TimeSpan.FromSeconds(2);
     options.Predicate = healthCheck => healthCheck.Tags.Contains("sample"); 
     options.Period = TimeSpan.FromSeconds(5);
 });
-builder.Services.AddSingleton<IHealthCheckPublisher, DefaultHealthCheckPublisher>();
-builder.Services.AddSingleton<IHealthCheckPublisher, AppHealthCheckPublisher>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -24,10 +25,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapHealthChecks("/healthz")
-    .RequireHost("*:6263");
-app.UseAuthorization();
+app.MapHealthChecks("/healthz", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.MapControllers();
+app.MapHealthChecksUI(st =>
+{
 
+});
 app.Run();
