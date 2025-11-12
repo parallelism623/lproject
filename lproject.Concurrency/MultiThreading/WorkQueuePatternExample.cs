598ec4cf9fa0971d9b2080f3f9b506646941063f
Hieu Truong Anh <hieu.truonganh@nashtechglobal.com>
2025-11-12 18:26:16 +0700

    Use case parallel

---
+++ lproject.Concurrency/MultiThreading/WorkQueuePatternExample.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Collections.Concurrent;
+using System.Net.Mail;
+
+namespace lproject.Concurrency.Multithreading;
+
+public class WorkQueuePatternExample
+{
+    private record MailInfo(
+         string from,
+         string subject,
+         string text,
+         string email);
+    BlockingCollection<MailInfo> _queue = new();
+    public void Start()
+    {
+        var thread = new Thread(BackGroundProc);
+        thread.Start();
+    }
+
+    public void Stop()
+    {
+        _queue.CompleteAdding();
+    }
+
+    public void MailMerge(
+        string from,
+        string subject,
+        string text,
+        (string email, string name)[] recipients)
+    {
+        foreach (var current in recipients)
+        {
+            _queue.Add(new MailInfo(
+            from,
+            subject,
+            text.Replace("{name}", current.name),
+            current.email));
+        }
+    }
+    public void BackGroundProc()
+    {
+        var sender = new SmtpClient("smtp.example.com");
+        foreach (var current in _queue.GetConsumingEnumerable())
+        {
+            try
+            {
+                var message = new MailMessage();
+                message.From = new MailAddress(current.from);
+
+                message.Subject = current.subject;
+                message.To.Add(new MailAddress(current.email));
+                message.Body = current.text;
+                sender.Send(message);
+            }
+            catch
+            {
+                LogFailure(current.email);
+            }
+        }
+    }
+    private void LogFailure(string message)
+    {
+
+    }
+}
--- lproject.Concurrency/Program.cs
+++ lproject.Concurrency/Program.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 
 Console.WriteLine("Start Main Thread");
-await TaskRunExample.RunInBackground();   
+List<int> values = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+SendMailItemsInParrallel.SendMailItemInMultipleThreadingByParallelClass(values);   
 Console.WriteLine("Stop Main Thread");
 
 
